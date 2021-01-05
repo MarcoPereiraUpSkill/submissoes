0cd4dab2d975819f29b6321c48211e1516d0faab
Marco Pereira <marco_epav@upskill.pt>
2021-01-05 10:06:43 +0000

    update projeto segmento-reta

--- Linguagem de programação Java/segmento_reta/src/main/java/com/mycompany/segmento_reta/SegmentoReta.java
+++ Linguagem de programação Java/segmento_reta/src/main/java/com/mycompany/segmento_reta/SegmentoReta.java
@@ -1,14 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.segmento_reta;
 
-/**
- *
- * @author marco
- */
 public class SegmentoReta {
+    private Ponto inicio;
+    private Ponto fim;
     
+    public void Ponto(Ponto inicio, Ponto fim){
+        this.inicio = inicio;
+        this.fim = fim;
+    }
+
+    public Ponto getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(Ponto inicio) {
+        this.inicio = inicio;
+    }
+
+    public Ponto getFim() {
+        return fim;
+    }
+
+    public void setFim(Ponto fim) {
+        this.fim = fim;
+    }
+
+    @Override
+    public String toString() {
+        return "SegmentoReta{" + "inicio=" + inicio + ", fim=" + fim + '}';
+    }
+    
+    public double determinarComprimento(){
+        return Math.sqrt((fim.getY() - inicio.getY()) * (fim.getY() - inicio.getY()) + 
+                (fim.getX() - inicio.getX()) * (fim.getX() - inicio.getX()));
+    }
+    
+    public double calcularDeclive(){
+        return (inicio.getY() - fim.getY()) / (inicio.getX() - fim.getX());
+    }
+    
+    public void deslocarSegmento(int dx, int dy){
+        inicio.moverPonto(dx, dy);
+        fim.moverPonto(dx, dy);
+    }
 }
