4d3f0ff5516b2d223460017d07266a51d20419c6
Marco Pereira <marco_epav@upskill.pt>
2021-01-12 10:14:47 +0000

    update ex_contador_cliques

--- Linguagem de programação Java/DataGUI/src/main/java/org/upskill/datagui/SceneController.java
+++ Linguagem de programação Java/DataGUI/src/main/java/org/upskill/datagui/SceneController.java
@@ -5,6 +5,7 @@
  */
 package org.upskill.datagui;
 
+import com.sun.media.sound.InvalidFormatException;
 import java.net.URL;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -25,7 +26,7 @@ import org.upskill.datagui.model.MesInvalidoException;
 /**
  * FXML Controller class
  *
- * 
+ *
  */
 public class SceneController implements Initializable {
 
@@ -35,6 +36,9 @@ public class SceneController implements Initializable {
     private TextField weekDayTxtField;
     @FXML
     private Label fullDateLabel;
+    
+    private int dia, mes, ano;
+    private Data data;
 
     /**
      * Initializes the controller class.
@@ -60,14 +64,33 @@ public class SceneController implements Initializable {
         weekDayTxtField.clear();
         fullDateLabel.setText("");
     }
-    
-    private Data readDate(){
-        String[] dateArgs = dateTxtField.getText().split("/", 3);
-        int ano = Integer.parseInt(dateArgs[0]);
-        int mes = Integer.parseInt(dateArgs[1]);
-        int dia = Integer.parseInt(dateArgs[2]);
-        Data data = new Data(ano, mes, dia);
-        
+
+    private Data readDate() {
+        try {
+            String[] dateArgs = dateTxtField.getText().split("/", 3);
+            ano = Integer.parseInt(dateArgs[0]);
+            mes = Integer.parseInt(dateArgs[1]);
+            dia = Integer.parseInt(dateArgs[2]);
+
+            data = new Data(ano, mes, dia);
+
+        } catch (DiaInvalidoException die) {
+            Alert alertDia = new Alert(AlertType.ERROR);
+            alertDia.setContentText("Dia " + dia + " não válido!");
+
+            alertDia.show();
+        } catch (MesInvalidoException mie) {
+            Alert alertMes = new Alert(AlertType.ERROR);
+            alertMes.setContentText("Mês " + mes + " não válido!");
+
+            alertMes.show();
+        } catch (RuntimeException e){
+            Alert alertFormat = new Alert(AlertType.ERROR);
+            alertFormat.setContentText("Formato inválido!");
+            
+            alertFormat.show();
+        }
+
         return data;
     }
 
