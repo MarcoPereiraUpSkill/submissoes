441f0889a9039f6c55e312e9a0f2a27280c557de
Marco Pereira <marco_epav@upskill.pt>
2021-01-12 09:31:54 +0000

    update ex_contador_cliques

--- Linguagem de programação Java/ex_contador_cliques/src/main/java/com/mycompany/ex_contador_cliques/FXMLController.java
+++ Linguagem de programação Java/ex_contador_cliques/src/main/java/com/mycompany/ex_contador_cliques/FXMLController.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.mycompany.ex_contador_cliques;
 
 import java.net.URL;
@@ -7,19 +12,29 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
+/**
+ * FXML Controller class
+ *
+ * @author marco
+ */
 public class FXMLController implements Initializable {
-    
+
     @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+    private Label clickNumberLabel;
+    private int numCliques = 0;
+
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void btnClickAddAction(ActionEvent event) {
+        numCliques++;
+        clickNumberLabel.setText("Número de Cliques: " + numCliques);
+    }
+    
 }
